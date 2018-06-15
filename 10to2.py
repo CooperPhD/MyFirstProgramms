893a39e7650708f07d425c3a27c0502a1af34973
Cooper Dorian <cooper.ph.dim@gmail.com>
2018-06-15 19:44:50 +0700

    four

--- 10to2.py
+++ 10to2.py
@@ -3,4 +3,5 @@ total = ''
 while nubmber: # пока number не равно 0
     total += str(nubmber%2)
     nubmber //=2 # nubmber = nubmber // 2
-print(total[::-1], 'stop') # переворачиваем строку
+print(total[::-1]) 
+print('Klop')# переворачиваем строку
