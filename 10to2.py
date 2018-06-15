cc938c4eaf5080b880c5b9a84fc6c722ce6a1d44
Cooper Dorian <cooper.ph.dim@gmail.com>
2018-06-15 19:02:38 +0700

    first commit

---
+++ 10to2.py
@@ -0,0 +1,6 @@
+nubmber = int(input())
+total = ''
+while nubmber: # пока number не равно 0
+    total += str(nubmber%2)
+    nubmber //=2 # nubmber = nubmber // 2
+print(total[::-1], 'stop time') # переворачиваем строку
