7ad50054a7cb459ab773677c5ae14b86f1cde208
Cooper Dorian <cooper.ph.dim@gmail.com>
2018-06-18 01:19:36 +0700

    number to Rome nubmer

--- file.py
+++ file.py
@@ -1,5 +1,22 @@
-year = int(input()) #Високосный ли год
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    print("YES")
-else:
-    print('NO')
+number = int(input())
+
+# Вычисляем десятки
+if number//10 <=3:
+    print('X' * (number// 10 - 5), end='')
+elif number//10 == 4:
+    print('XL', end='')
+elif number//10 <=8:
+    print('L' + 'X' * (number// 10 - 5), end='')
+elif number//10 == 9:
+    print('XC', end='')
+
+number = number % 10
+# Вычисляем еденицы
+if  0 < number <= 3:
+    print('I' * number)
+elif number == 4:
+    print('IV')
+elif number <=8:
+    print('V' + 'I' * (number - 5))
+elif number == 9:
+    print('IX')
