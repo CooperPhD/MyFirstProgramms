1b77fbe11bdddb34ad249a68c9f40f4a63d1c576
Cooper Dorian <cooper.ph.dim@gmail.com>
2018-06-17 20:19:52 +0700

    Функции split и map

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
-name = input()
-if name:
-    print('Hello,', name)
-else:
-    print('Hello, anon')
+first_last_name = str(input()) #вводим два слова
+print(type(first_last_name.split())) #выводим список  
+a, b = map(len, first_last_name.split()) #присваеваем
+print(a, b)
+
+
