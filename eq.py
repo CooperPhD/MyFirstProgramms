53a23a84f6bcc1d064ddf6bd50c2ee24cc40e2e5
Cooper Dorian <cooper.ph.dim@gmail.com>
2018-06-24 23:03:32 +0700

    fu*$ math!

--- eq.py
+++ eq.py
@@ -1,11 +1,4 @@
-def gcd(a, b, x):
-    if a == 0 or b == 0:
-        return max(a, b)
-    elif a > b:
-        return gcd(a - b, b, x + 1)
-    else:
-        return gcd(a, b - a, x + 1)
-x = 0
-a = int(input())
-b = int(input())
-print(gcd(a, b, x))
+q = 0
+for i in range(1,101):
+    q += i**3
+    print(q)
---
+++ eq2.py
@@ -0,0 +1,4 @@
+s = 0
+for k in range(1, 123456790, 2):
+    s = s + k
+print(s)
